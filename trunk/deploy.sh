f9b999404c98ec22ab66cc01dbee9acc21a928cd
kirill.kovalchuk.vladimirovich@gmail.com <kirill.kovalchuk.vladimirovich@gmail.com@9d21389f-95c4-d4d3-f2de-aff1f48b621a>
2013-02-11 11:29:32 +0000

    imporve deployment script

--- trunk/deploy.sh
+++ trunk/deploy.sh
@@ -3,5 +3,7 @@
 cp -R src src2
 /bin/find src2 -name .svn -exec rm -Rf {} \;
 rm -Rf src2/.idea
-ncftpput -R -v -u $TETRIS_USERNAME -p $TETRIS_PASSWORD $TETRIS_HOSTNAME . src2
+cd src2
+ncftpput -R -v -u $TETRIS_USERNAME -p $TETRIS_PASSWORD $TETRIS_HOSTNAME . .
+cd ..
 rm -Rf src2
