f0034f6bd2342340c17ab6961112a5294d0dcdab
kirill.kovalchuk.vladimirovich <kirill.kovalchuk.vladimirovich@gmail.com>
2013-02-13 10:35:33 +0000

    fix bug with rendering first figure; figures are shifted if no possibility to rotate as is

--- src/js/figure.js
+++ src/js/figure.js
@@ -1,8 +1,8 @@
 var Figure = function (gamearea) {
-    this.pattern = PATTERNS[parseInt(Math.random()*PATTERNS.length)];
-    this.direction = parseInt(Math.random()*this.pattern.patterns.length);
-    this.x = this.pattern.startx;
-    this.y = 0;
+    this.patterns = PATTERNS[parseInt(Math.random()*PATTERNS.length)];
+    this.direction = parseInt(Math.random()*this.patterns.length);
+    this.x = this.patterns[this.direction].startx;
+    this.y = this.patterns[this.direction].starty;
 
     this.move = function (newx, newy, new_direction) {
         if (!this.can_moved(newx, newy, new_direction))
@@ -17,10 +17,10 @@ var Figure = function (gamearea) {
     };
 
     this.can_moved = function (newx, newy, new_direction) {
-        var patterns = this.pattern.patterns[new_direction];
-        for (var i=0; i<patterns.length; i++)
-            for (var j=0; j<patterns[i].length; j++) {
-                if (patterns[i][j] == 1) {
+        var pattern = this.patterns[new_direction].pattern;
+        for (var i=0; i<pattern.length; i++)
+            for (var j=0; j<pattern[i].length; j++) {
+                if (pattern[i][j] == 1) {
                     var y = newy+i;
                     var x = newx+j;
                     if (!(0 <= x && x < WIDTH && 0 <= y && y < HEIGHT))
@@ -35,10 +35,10 @@ var Figure = function (gamearea) {
     };
 
     this.render = function (sel, commit) {
-        var patterns = this.pattern.patterns[this.direction];
-        for (var i=0; i<patterns.length; i++)
-            for (var j=0; j<patterns[i].length; j++) {
-                if (patterns[i][j] == 1)
+        var pattern = this.patterns[this.direction].pattern;
+        for (var i=0; i<pattern.length; i++)
+            for (var j=0; j<pattern[i].length; j++) {
+                if (pattern[i][j] == 1)
                     if (sel)
                         gamearea.cells[this.y+i][this.x+j].sel(commit);
                     else
@@ -63,7 +63,16 @@ var Figure = function (gamearea) {
         return this.move(this.x - 1, this.y, this.direction);
     };
 
-    this.rotate = function () {
-        return this.move(this.x, this.y, (this.direction + 1) % this.pattern.patterns.length);
+    this.rotate = function (delta) {
+        var new_direction = (this.direction + delta + this.patterns.length) % this.patterns.length;
+        if (!this.move(this.x, this.y, new_direction))
+            return false;
+
+        var shifts = this.patterns[this.direction].shifts;
+        for (var i=0; i<shifts.length; i++)
+            if (!this.move(this.x+shifts[i], this.y, new_direction))
+                return false;
+
+        return true;
     };
 };
--- src/js/patterns.js
+++ src/js/patterns.js
@@ -1,141 +1,215 @@
 var PATTERNS = [
-    {
-        startx: 4,
-        patterns:[
-            [
+    [
+        {
+            startx: 4,
+            starty: 0,
+            shifts: [],
+            pattern: [
                 [1, 1],
                 [1, 1]
             ]
-        ]
-    },
-    {
-        startx: 4,
-        patterns:[
-            [
+        }
+    ],
+    [
+        {
+            startx: 4,
+            starty: 0,
+            shifts: [-1],
+            pattern:[
                 [0, 1, 0],
                 [1, 1, 0],
                 [0, 1, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 4,
+            starty: 0,
+            shifts: [],
+            pattern:[
                 [0, 1, 0],
                 [1, 1, 1],
                 [0, 0, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 4,
+            starty: 0,
+            shifts: [1],
+            pattern:[
                 [0, 1, 0],
                 [0, 1, 1],
                 [0, 1, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 4,
+            starty: -1,
+            shifts: [],
+            pattern:[
                 [0, 0, 0],
                 [1, 1, 1],
                 [0, 1, 0]
             ]
-        ]
-    },
-    {
-        startx: 4,
-        patterns:[
-            [
+        }
+    ],
+    [
+        {
+            startx: 4,
+            starty: 0,
+            shifts: [-1],
+            pattern:[
                 [0, 1, 0],
                 [1, 1, 0],
                 [1, 0, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 4,
+            starty: -1,
+            shifts: [],
+            pattern:[
                 [0, 0, 0],
                 [1, 1, 0],
                 [0, 1, 1]
             ]
-        ]
-    },
-    {
-        startx: 4,
-        patterns:[
-            [
+        }
+    ],
+    [
+        {
+            startx: 4,
+            starty: 0,
+            shifts: [1],
+            pattern:[
                 [0, 1, 0],
                 [0, 1, 1],
                 [0, 0, 1]
-            ],
-            [
-                [0, 1, 1],
-                [1, 1, 0],
-                [0, 0, 0]
             ]
-        ]
-    },
-    {
-        startx: 3,
-        patterns:[
-            [
+        },
+        {
+            startx: 4,
+            starty: -1,
+            shifts: [],
+            pattern:[
+                [0, 0, 0],
+                [0, 1, 1],
+                [1, 1, 0]
+            ]
+        }
+    ],
+    [
+        {
+            startx: 3,
+            starty: 0,
+            shifts: [-1, 1, 2],
+            pattern:[
                 [0, 0, 1, 0],
                 [0, 0, 1, 0],
                 [0, 0, 1, 0],
                 [0, 0, 1, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 3,
+            starty: -1,
+            shifts: [],
+            pattern:[
                 [0, 0, 0, 0],
                 [1, 1, 1, 1],
                 [0, 0, 0, 0],
                 [0, 0, 0, 0]
             ]
-        ]
-    },
-    {
-        startx: 3,
-        patterns:[
-            [
+        }
+    ],
+    [
+        {
+            startx: 3,
+            starty: 0,
+            shifts: [-1, 1],
+            pattern:[
                 [0, 0, 1, 0],
                 [0, 0, 1, 0],
                 [0, 1, 1, 0],
                 [0, 0, 0, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 3,
+            starty: -1,
+            shifts: [1],
+            pattern:[
                 [0, 0, 0, 0],
                 [0, 1, 0, 0],
                 [0, 1, 1, 1],
                 [0, 0, 0, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 3,
+            starty: -1,
+            shifts: [-1, 1],
+            pattern:[
                 [0, 0, 0, 0],
                 [0, 1, 1, 0],
                 [0, 1, 0, 0],
                 [0, 1, 0, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 3,
+            starty: -1,
+            shifts: [-1],
+            pattern:[
                 [0, 0, 0, 0],
                 [1, 1, 1, 0],
                 [0, 0, 1, 0],
                 [0, 0, 0, 0]
             ]
-        ]
-    },
-    {
-        startx: 3,
-        patterns:[
-            [
+        }
+    ],
+    [
+        {
+            startx: 3,
+            starty: 0,
+            shifts: [-1, 1],
+            pattern:[
                 [0, 1, 0, 0],
                 [0, 1, 0, 0],
                 [0, 1, 1, 0],
                 [0, 0, 0, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 3,
+            starty: -1,
+            shifts: [1],
+            pattern:[
                 [0, 0, 0, 0],
                 [0, 1, 1, 1],
                 [0, 1, 0, 0],
                 [0, 0, 0, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 3,
+            starty: -1,
+            shifts: [-1, 1],
+            pattern:[
                 [0, 0, 0, 0],
                 [0, 1, 1, 0],
                 [0, 0, 1, 0],
                 [0, 0, 1, 0]
-            ],
-            [
+            ]
+        },
+        {
+            startx: 3,
+            starty: -1,
+            shifts: [-1],
+            pattern:[
                 [0, 0, 0, 0],
                 [0, 0, 1, 0],
                 [1, 1, 1, 0],
                 [0, 0, 0, 0]
             ]
-        ]
-    }
+        }
+    ]
 ];
