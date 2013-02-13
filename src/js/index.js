a0b5c7280fe9cfef7e13571f026454f50b690464
kirill.kovalchuk.vladimirovich <kirill.kovalchuk.vladimirovich@gmail.com>
2013-02-13 09:50:45 +0000

    fix bug with cube; improve keyboard behaviour

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,15 @@ var WIDTH = 10;
 var HEIGHT = 20;
 var FPS = 60; // amount of frames per second
 var INITIAL_SPEED = 1; // cells to fall per second
-var KEYBOARD_SPEED = 20; // cells to move per second when key on a keyboard is pressed
-var DOWN_KEYBOARD_SPEED = 40; // cells to move per second when down key on a keyboard is  pressed
+
+// keyboard settings
+var KEYBOARD_SPEED = {};
+KEYBOARD_SPEED[KEY_UP] = undefined; // make just one action, don't repeat when up key is hold down
+// first number - amount of cells to move per second when key on a keyboard is hold down
+// second number - amount of milleseconds passed since first keydown event till starting moving when key is hold down
+KEYBOARD_SPEED[KEY_LEFT] = [30, 100];
+KEYBOARD_SPEED[KEY_RIGHT] = [30, 100];
+KEYBOARD_SPEED[KEY_DOWN] = [40, 0];
 
 $(function() {
     var mainarea = new Gamearea();
