a6430eced237eae1d875306c8d9febadda9053fa
kirill.kovalchuk.vladimirovich@gmail.com <kirill.kovalchuk.vladimirovich@gmail.com@9d21389f-95c4-d4d3-f2de-aff1f48b621a>
2013-02-14 07:51:38 +0000

    speed added

--- trunk/src/js/stat.js
+++ trunk/src/js/stat.js
@@ -9,5 +9,5 @@ function get_scores(level) { return Math.pow(level-1,2) * MULTIPLIER; }
 var Stat = function () {
     this.scores = ko.observable('scores');
     this.level = ko.computed(function() {return get_level(this.scores())}, this);
-    this.percent_completed = ko.computed(function(){ var floor = get_scores(this.level()); return (this.scores() - floor) / (get_scores(this.level()+1) - floor) * 100; }, this);
+    this.percent_completed = ko.computed(function(){ var floor = get_scores(this.level()); return ((this.scores() - floor) / (get_scores(this.level()+1) - floor) * 100) + '%'; }, this);
 };
