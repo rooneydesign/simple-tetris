3be4a51ec743ef3d52fec69350c95e694a1e5ba0
kirill.kovalchuk.vladimirovich <kirill.kovalchuk.vladimirovich@gmail.com>
2013-02-14 09:36:50 +0000

    fix color setting when selecting

--- src/js/cell.js
+++ src/js/cell.js
@@ -3,6 +3,7 @@ var Cell = function (x, y, $div) {
     this.color = 'transparent';
 
     this.sel = function (color, commit) {
+	this.color = color;
         $div.css('background-color', color);
         if (commit)
             this.committed = true;
@@ -10,6 +11,7 @@ var Cell = function (x, y, $div) {
     };
 
     this.unsel = function(commit) {
+        this.color = 'transparent';
         $div.css('background-color', 'transparent');
         if (commit)
             this.committed = false;
