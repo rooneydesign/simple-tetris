e3fa70b2226a79eebaef89bdc99520d747caac0b
kirill.kovalchuk.vladimirovich@gmail.com <kirill.kovalchuk.vladimirovich@gmail.com@9d21389f-95c4-d4d3-f2de-aff1f48b621a>
2013-02-17 13:07:29 +0000

    amount of fps is adoptable to cpu now

--- trunk/src/js/stat.js
+++ trunk/src/js/stat.js
@@ -3,4 +3,5 @@ var Stat = function () {
     this.scores = ko.observable('scores');
     this.level = ko.computed(function() {return get_level(this.scores())}, this);
     this.percent_completed = ko.computed(function(){ var floor = get_scores(this.level()); return ((this.scores() - floor) / (get_scores(this.level()+1) - floor) * 100) + '%'; }, this);
+    this.fps = ko.observable('fps');
 };
